6e1f412d3b83964f92fb7ee8370142e0ab519464
hyhdb <kkminwoo@gmail.com>
2026-04-12 20:47:22 +0900

    feat: StockService 수정

--- src/main/java/com/back/together02be/stock/service/StockService.java
+++ src/main/java/com/back/together02be/stock/service/StockService.java
@@ -1,5 +1,6 @@
 package com.back.together02be.stock.service;
 
+import com.back.together02be.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -7,4 +8,21 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public List<StockListResponse> getStocks() {
+        return stockRepository.findByIsActiveTrue()
+                .stream()
+                .map(stock -> new StockListResponse(
+                        stock.getId(),
+                        stock.getStockCode(),
+                        stock.getStockName(),
+                        stock.getMarket(),
+                        null,
+                        null,
+                        null
+                ))
+                .toList();
+    }
 }
