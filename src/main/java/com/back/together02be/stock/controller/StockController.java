13a4f8a53e6418a7d41af9905f7a4d5ac346c194
hyhdb <kkminwoo@gmail.com>
2026-04-12 20:50:51 +0900

    feat: StockController 수정

--- src/main/java/com/back/together02be/stock/controller/StockController.java
+++ src/main/java/com/back/together02be/stock/controller/StockController.java
@@ -1,5 +1,7 @@
 package com.back.together02be.stock.controller;
 
+import com.back.together02be.stock.service.StockService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/stocks")
 @Tag(name = "StockController", description = "주식 API")
 public class StockController {
+
+    private final StockService stockService;
+
+    @GetMapping
+    public List<StockListResponse> getStocks() {
+        return stockService.getStocks();
+    }
 }
