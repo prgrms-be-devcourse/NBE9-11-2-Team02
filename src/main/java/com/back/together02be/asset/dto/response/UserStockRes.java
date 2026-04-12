35604ead4c71f12a77dffb64188281b326a15012
jinwoo <realize2578@gmail.com>
2026-04-12 20:43:58 +0900

    feat: 보유 종목 조회 기능 구현
    # Conflicts:
    #	src/main/java/com/back/together02be/asset/repository/UserStockRepository.java

---
+++ src/main/java/com/back/together02be/asset/dto/response/UserStockRes.java
@@ -0,0 +1,19 @@
+package com.back.together02be.asset.dto.response;
+
+import com.back.together02be.asset.enitity.UserStock;
+
+public record UserStockRes(
+        String stockCode,
+        String stockName,
+        Long quantity,
+        Long averagePrice
+) {
+    public static UserStockRes from(UserStock userStock) {
+        return new UserStockRes(
+                userStock.getStock().getStockCode(),
+                userStock.getStock().getStockName(),
+                userStock.getQuantity(),
+                userStock.getAveragePrice()
+        );
+    }
+}
--- src/main/java/com/back/together02be/asset/repository/UserStockRepository.java
+++ src/main/java/com/back/together02be/asset/repository/UserStockRepository.java
@@ -1,5 +1,6 @@
 package com.back.together02be.asset.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import com.back.together02be.asset.enitity.UserStock;
 
 public interface UserStockRepository extends JpaRepository<UserStock, Long> {
 	Optional<UserStock> findByUsersIdAndStockId(Long usersId, Long stockId);
+    List<UserStock> findAllByUsersId(Long users_id);
 }
