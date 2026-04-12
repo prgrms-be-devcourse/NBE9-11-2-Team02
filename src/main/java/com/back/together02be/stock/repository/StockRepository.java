a28b84456c9cd86d021e2b6ddc6fa207b6f61850
hyhdb <kkminwoo@gmail.com>
2026-04-12 20:44:01 +0900

    feat: StockRepository 수정

--- src/main/java/com/back/together02be/stock/repository/StockRepository.java
+++ src/main/java/com/back/together02be/stock/repository/StockRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.back.together02be.stock.entity.Stock;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    List<Stock> findByIsActiveTrue();
+
 }
