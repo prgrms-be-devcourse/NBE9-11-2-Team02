1feca65060de5166d4760f709315224f0cec7530
hyhdb <kkminwoo@gmail.com>
2026-04-17 11:20:47 +0900

    feat(stock): 캐시 기반 단일 종목 가격 조회

--- src/main/java/com/back/together02be/stock/controller/StockController.java
+++ src/main/java/com/back/together02be/stock/controller/StockController.java
@@ -1,24 +1,18 @@
 package com.back.together02be.stock.controller;
 
-import java.util.List;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
 import com.back.together02be.global.apiRes.ApiRes;
 import com.back.together02be.stock.dto.StockListRes;
 import com.back.together02be.stock.dto.response.StockPriceRes;
 import com.back.together02be.stock.service.StockService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
