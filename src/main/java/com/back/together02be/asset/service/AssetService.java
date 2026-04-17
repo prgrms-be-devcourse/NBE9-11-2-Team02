81071b0ea12cd24fb70a432af275af732ad99d80
realize2578 <realize2578@gmail.com>
2026-04-17 15:10:16 +0900

    Feature/pf 01 (#29)
    * feat: 보유 종목 조회 기능 구현
    
    # Conflicts:
    #	src/main/java/com/back/together02be/asset/repository/UserStockRepository.java
    
    * chore: asset entity 디렉토리 enitity->entity로 변경

--- src/main/java/com/back/together02be/asset/service/AssetService.java
+++ src/main/java/com/back/together02be/asset/service/AssetService.java
@@ -1,10 +1,22 @@
 package com.back.together02be.asset.service;
 
+import com.back.together02be.asset.dto.response.UserStockRes;
+import com.back.together02be.asset.repository.UserStockRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AssetService {
+
+    private final UserStockRepository userStockRepository;
+
+    public List<UserStockRes> getUserStocks(Long userId) {
+        return userStockRepository.findAllByUsersId(userId).stream()
+                .map(UserStockRes::from)
+                .toList();
+    }
 }
