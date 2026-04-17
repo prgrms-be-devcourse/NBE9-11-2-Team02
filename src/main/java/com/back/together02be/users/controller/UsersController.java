ddeb7a0bd99bf553cffdbfe18333566a2f7a3cc4
sat7312 <sat7312@gmail.com>
2026-04-17 09:45:03 +0900

    feat: 회원가입 BCrypt 암호화 및 Security 기본 설정 추가

--- src/main/java/com/back/together02be/users/controller/UsersController.java
+++ src/main/java/com/back/together02be/users/controller/UsersController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -26,7 +27,7 @@ public class UsersController {
 
     @PostMapping("/signup")
     @Operation(summary = "회원 가입")
-    public ResponseEntity<ApiRes<Void>> signup(@RequestBody SignupReq req) {
+    public ResponseEntity<ApiRes<Void>> signup(@RequestBody @Valid SignupReq req) {
 
         usersService.signup(req);
         return ResponseEntity.ok(
