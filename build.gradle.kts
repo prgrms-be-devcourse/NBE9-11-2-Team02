c93aad7cf48bb996258dca338a7823cf65b368d4
gioJioh <sjgio0515@naver.com>
2026-04-18 17:12:10 +0900

    feat: 특정 종목 차트 3M, 1Y기간 조회

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+
+    implementation ("org.springframework.boot:spring-boot-starter-cache")
+    implementation ("com.github.ben-manes.caffeine:caffeine")
 }
 
 tasks.withType<Test> {
