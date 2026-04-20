10bbe82a53ecf5c716557ef8b618951d940c432a
gmltmdu <dbtmdgml03@naver.com>
2026-04-20 15:45:13 +0900

    Squashed commit of the following: dev <- feature/ma-01 commit 9852bbab032fab2b4742d1cf7d65b9e03d6adf21 Author: gmltmdu <dbtmdgml03@naver.com> Date:   Mon Apr 20 15:31:45 2026 +0900
        fix: 함수명 수정
    
    commit 2ca3ec83d6f4f93012d6d1738cc62dc3e32ad437
    Author: gmltmdu <dbtmdgml03@naver.com>
    Date:   Mon Apr 13 14:37:44 2026 +0900
    
        feature: 종목 코드 및 수량 출력 API 구현
    
    commit 878b0b259a922372a3b9815b78e6adb43b44904c
    Author: gmltmdu <dbtmdgml03@naver.com>
    Date:   Sun Apr 12 22:00:52 2026 +0900
    
        feature: 총 매수금 조회 API 구현

--- src/main/java/com/back/together02be/asset/repository/UserStockRepository.java
+++ src/main/java/com/back/together02be/asset/repository/UserStockRepository.java
@@ -2,7 +2,7 @@ package com.back.together02be.asset.repository;
 
 import com.back.together02be.asset.entity.UserStock;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.back.together02be.asset.entity.UserAccount;
 import java.util.List;
 import java.util.Optional;
 
